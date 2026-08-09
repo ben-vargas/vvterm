befce7c62b8a2a9004d8fcbcbf18c696fbe1525c
Wiedy Mi <42713027+wiedymi@users.noreply.github.com>
2026-08-10 06:35:09 +0800

    Move terminal snapshot persistence to Infrastructure

--- VVTerm/Features/TerminalSessions/Application/TerminalTabSnapshotStore.swift
+++ VVTerm/Features/TerminalSessions/Application/TerminalTabSnapshotStore.swift
@@ -6,26 +6,3 @@ protocol TerminalTabSnapshotStoring: AnyObject {
     func saveSnapshotData(_ data: Data)
     func removeSnapshotData()
 }
-
-@MainActor
-final class UserDefaultsTerminalTabSnapshotStore: TerminalTabSnapshotStoring {
-    private let defaults: UserDefaults
-    private let key: String
-
-    init(defaults: UserDefaults, key: String) {
-        self.defaults = defaults
-        self.key = key
-    }
-
-    func loadSnapshotData() -> Data? {
-        defaults.data(forKey: key)
-    }
-
-    func saveSnapshotData(_ data: Data) {
-        defaults.set(data, forKey: key)
-    }
-
-    func removeSnapshotData() {
-        defaults.removeObject(forKey: key)
-    }
-}
