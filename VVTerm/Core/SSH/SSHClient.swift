0dbc2ac61d21141aedf61ad99f9fa603fbd24b34
Wiedy Mi <42713027+wiedymi@users.noreply.github.com>
2026-08-10 20:34:41 +0800

    Split SSH shell handle

--- VVTerm/Core/SSH/SSHClient.swift
+++ VVTerm/Core/SSH/SSHClient.swift
@@ -6,34 +6,6 @@ import MoshBootstrap
 
 // MARK: - SSH Client using libssh2
 
-nonisolated struct ShellHandle: Sendable {
-    let id: UUID
-    let stream: TerminalOutputStream
-    let transportState: ShellTransportState
-    let origin: ShellStartOrigin
-
-    var transport: ShellTransport { transportState.transport }
-    var fallbackReason: MoshFallbackReason? { transportState.fallbackReason }
-    var fallbackDiagnostics: MoshFallbackDiagnostics? { transportState.fallbackDiagnostics }
-
-    init(
-        id: UUID,
-        stream: TerminalOutputStream,
-        transportState: ShellTransportState = .ssh,
-        origin: ShellStartOrigin = .fresh
-    ) {
-        self.id = id
-        self.stream = stream
-        self.transportState = transportState
-        self.origin = origin
-    }
-}
-
-nonisolated enum ShellStartOrigin: Equatable, Sendable {
-    case fresh
-    case restored
-}
-
 actor SSHClient {
     enum LifecyclePhase: Equatable, Sendable {
         case disconnected
