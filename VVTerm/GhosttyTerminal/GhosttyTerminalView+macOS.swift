2ac58dea7e8d3ab212652e1eb30a6d8628d0db78
Wiedy Mi <42713027+wiedymi@users.noreply.github.com>
2026-08-10 21:52:54 +0800

    Split macOS terminal IME integration

--- VVTerm/GhosttyTerminal/GhosttyTerminalView+macOS.swift
+++ VVTerm/GhosttyTerminal/GhosttyTerminalView+macOS.swift
@@ -93,7 +93,7 @@ class GhosttyTerminalView: NSView, NSUserInterfaceValidations {
 
     // MARK: - Handler Components
 
-    private var imeHandler: GhosttyIMEHandler!
+    var imeHandler: GhosttyIMEHandler!
     private var inputHandler: GhosttyInputHandler!
     private let renderingSetup = GhosttyRenderingSetup()
 
@@ -1054,57 +1054,6 @@ class GhosttyTerminalView: NSView, NSUserInterfaceValidations {
             surface.sendText(String(controlChar))
             requestRender()
         }
-    }
-}
-
-// MARK: - NSTextInputClient Implementation
-
-/// NSTextInputClient protocol conformance for IME (Input Method Editor) support
-extension GhosttyTerminalView: NSTextInputClient {
-    func insertText(_ string: Any, replacementRange: NSRange) {
-        imeHandler.insertText(string, replacementRange: replacementRange)
-    }
-
-    func setMarkedText(_ string: Any, selectedRange: NSRange, replacementRange: NSRange) {
-        imeHandler.setMarkedText(string, selectedRange: selectedRange, replacementRange: replacementRange)
-    }
-
-    func unmarkText() {
-        imeHandler.unmarkText()
-    }
-
-    func selectedRange() -> NSRange {
-        return imeHandler.selectedRange()
-    }
-
-    func markedRange() -> NSRange {
-        return imeHandler.markedRange()
-    }
-
-    func hasMarkedText() -> Bool {
-        return imeHandler.hasMarkedText
-    }
-
-    func attributedSubstring(forProposedRange range: NSRange, actualRange: NSRangePointer?) -> NSAttributedString? {
-        return imeHandler.attributedSubstring(forProposedRange: range, actualRange: actualRange)
-    }
-
-    func validAttributesForMarkedText() -> [NSAttributedString.Key] {
-        return imeHandler.validAttributesForMarkedText()
-    }
-
-    func firstRect(forCharacterRange range: NSRange, actualRange: NSRangePointer?) -> NSRect {
-        return imeHandler.firstRect(
-            forCharacterRange: range,
-            actualRange: actualRange,
-            viewFrame: frame,
-            window: window,
-            surface: surface?.unsafeCValue
-        )
-    }
-
-    func characterIndex(for point: NSPoint) -> Int {
-        return imeHandler.characterIndex(for: point)
     }
 }
 
